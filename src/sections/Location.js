e05c97e20ae162a6e06b678cdddb1b10c9792293
pramodpawar11 <kingpramodpawar511@gmail.com>
2024-02-13 10:14:49 +0530

    Crate a locations section

--- src/sections/Location.js
+++ src/sections/Location.js
@@ -1,7 +1,24 @@
 
 const Location = () => {
   return (
-    <div>Location</div>
+    <section className="h-screen flex items-center justify-center ">
+      <div className="absolute inset-0 overflow-hidden padding-t  ">
+        <img className="w-full h-full object-cover object-center" src="https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L2lzMTYwNjItaW1hZ2Uta3d2eWZrd3IuanBn.jpg" />
+      </div>
+      <div className="absolute flex items-center justify-center flex-col">
+        <div className="text-center">
+          <span className="  text-white sm:text-4xl text-3xl font-bold font-montserrat">Get food</span>
+          <span className=" text-coral-red sm:text-4xl text-3xl font-bold font-montserrat pr-2 pl-2">delivery</span>
+          <span className=" text-white sm:text-4xl text-3xl font-bold font-montserrat">and more</span>
+        </div>
+
+
+        <button className="rounded-2xl py-2 px-[30%] mt-5 cursor-pointer text-white font-bold bg-coral-red outline-none border-none hover:bg-red-500">Select State</button>
+
+
+      </div>
+
+    </section>
   )
 }
 
--- src/sections/Nav.js
+++ src/sections/Nav.js
@@ -4,10 +4,10 @@ const Nav = () => {
     <header className="padding-x pb-8 absolute w-full z-10">
     <nav className="flex justify-between items-center max-container">
         <a href="/">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT281auN11e-OaZTPUuyEi3ZvIER86v44RnIlEZJn7ADoUA1-baG53pAKPtxohCoWP_0pE&usqp=CAU" alt="logo" width={100} height={40} className="m-0 w-[100px] h-[100px]"/>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT281auN11e-OaZTPUuyEi3ZvIER86v44RnIlEZJn7ADoUA1-baG53pAKPtxohCoWP_0pE&usqp=CAU" alt="logo" width={90} height={30} className="m-0 w-[90px] h-[90px]"/>
         </a>
         <ul>
-            <button className= "border-2 rounded-full py-1 px-3 text-blue-700 font-bold">SignIn</button>
+            <button className= "border-coral-red border-2 rounded-full py-1.5 px-3.5 text-blue-700 font-bold text-sm">Sign in</button>
         </ul>
     </nav>
 
